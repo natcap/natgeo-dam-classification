6d654b37670de1038b4bcd81700d6e987c86605d
Rich Sharp <richpsharp@gmail.com>
2019-01-28 22:54:33 -0800

    trying to list templates and such

--- data_server.py
+++ data_server.py
@@ -1,9 +1,10 @@
 """NatGeo flask data server."""
+import os
 import sys
 import logging
 
 from flask import Flask
-
+from flask import render_template
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,16 +15,33 @@ logging.basicConfig(
         ' [%(funcName)s:%(lineno)d] %(message)s'),
     stream=sys.stdout)
 
-APP = Flask(__name__, static_url_path='/workspace/sentinel_granules/')
+APP = Flask(__name__)
 
 @APP.route('/')
 def index():
     """Entry page."""
     try:
-        return '<img src="workspace/sentinel_granules/L1C_T10UEB_A012108_20171016T193021/T10UEB_20171016T192401_B02_grand_19.png">'
+        path = './workspace/sentinel_granules'
+        return render_template('index.html', tree=make_tree(path))
     except Exception as e:
         return str(e)
 
+
+def make_tree(path):
+    """Make a directory tree struct."""
+    tree = dict(name=os.path.basename(path), children=[])
+    try:
+        lst = os.listdir(path)
+    except OSError:
+        pass  #ignore errors
+    else:
+        for name in lst:
+            fn = os.path.join(path, name)
+            if os.path.isdir(fn):
+                tree['children'].append(make_tree(fn))
+            else:
+                tree['children'].append(dict(name=name))
+    return tree
 
 if __name__ == '__main__':
     APP.run(host='0.0.0.0', port=8080)
