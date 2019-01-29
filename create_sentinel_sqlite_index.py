b8586afcc5c5a3892efea8c15305e9a7f020aba5
Rich Sharp <richpsharp@gmail.com>
2019-01-28 21:39:39 -0800

    making task list a list

--- create_sentinel_sqlite_index.py
+++ create_sentinel_sqlite_index.py
@@ -167,38 +167,31 @@ def schedule_grand_sentinel_extraction(
                     target_path_list=[manifest_path],
                     task_name=f'fetch {manifest_url}')
 
-                for data_tag in ('IMG_DATA_Band_TCI_Tile1_Data',):
-                    complete_token_path = os.path.join(
-                        granule_dir, f'{data_tag}.COMPLETE')
-                    granule_task_fetch = task_graph.add_task(
-                        func=fetch_tile_and_bound_data,
-                        args=(
-                            manifest_path, url_prefix, data_tag,
-                            'grand_%d' % grand_id, grand_bb, wgs84_srs_wkt,
-                            granule_dir, complete_token_path),
-                        dependent_task_list=[manifest_task_fetch],
-                        target_path_list=[complete_token_path],
-                        task_name=f'fetch {data_tag}')
+                fetch_tile_and_bound_data(
+                    task_graph, [manifest_task_fetch], manifest_path,
+                    url_prefix, 'grand_%d' % grand_id, grand_bb,
+                    wgs84_srs_wkt, granule_dir)
+                break
+        break
 
 
 def fetch_tile_and_bound_data(
-        manifest_path, url_prefix, data_tag, bound_id_suffix, bounding_box,
-        bounding_box_ref_wkt, target_dir, complete_token_path):
+        task_graph, dependent_task_list, manifest_path, url_prefix,
+        bound_id_suffix, bounding_box, bounding_box_ref_wkt, target_dir):
     """Fetch tile from url defined in manifest_path.
 
     Parameters:
+        task_graph (TaskGraph): taskgraph object to schedule against.
+        dependent_task_list (list): list of tasks that need to be executed
+            first before any scheduling.
         manifest_path (str): path to a SAFE file describing sentinel data.
         url_prefix (str): url to prepend to any URLs needed by this function.
-        data_tag (str): embedded dataObject tag to fetch.
         bound_id_suffix (str): this string is appended to the fetched file
             to define the clipped version by the bounding box.
         bounding_box (tuple): 4 tuple of the form [xmin, ymin, xmax, ymax].
         bounding_box_ref_wkt (str): wkt coordinate reference system for
             `bounding_box`.
         target_dir (str): path to directory which to download the data object.
-        complete_token_path (str): path to a file to touch when fetch is
-            complete. This helps avoid repeated reexecution when target path
-            is otherwise unknown.
 
     Returns:
         None
@@ -224,9 +217,15 @@ def fetch_tile_and_bound_data(
         while True:
             try:
                 fetch_attempt += 1
-                urllib.request.urlretrieve(granule_url, granule_path)
+                fetch_task = task_graph.add_task(
+                    func=urllib.request.urlretrieve,
+                    args=(granule_url, granule_path),
+                    target_path_list=[granule_path],
+                    dependent_task_list=dependent_task_list,
+                    task_name=f'fetch {granule_path}')
+                fetch_task.join()
                 break
-            except urllib.error.HTTPError:
+            except:
                 if fetch_attempt == 5:
                     raise
                 sleep_time = fetch_attempt * 5.0
@@ -241,15 +240,24 @@ def fetch_tile_and_bound_data(
         # this will be a GeoTIFF, hence the .tif suffix
         clipped_raster_path = (
             f'{os.path.splitext(granule_path)[0]}_{bound_id_suffix}.tif')
-        pygeoprocessing.warp_raster(
-            granule_path, granule_raster_info['pixel_size'],
-            clipped_raster_path, 'near', target_bb=target_bounding_box)
-        subprocess.run(
-            ['gdal_translate', '-of', 'PNG', clipped_raster_path,
-             f'{os.path.splitext(clipped_raster_path)[0]}.png'])
-
-        with open(complete_token_path, 'w') as complete_token_file:
-            complete_token_file.write(granule_url)
+        warp_task = task_graph.add_task(
+            func=pygeoprocessing.warp_raster,
+            args=(
+                granule_path, granule_raster_info['pixel_size'],
+                clipped_raster_path, 'near'),
+            kwargs={'target_bb': target_bounding_box},
+            target_path_list=[clipped_raster_path],
+            dependent_task_list=[fetch_task],
+            task_name=f'clip {clipped_raster_path}')
+        png_path = f'{os.path.splitext(clipped_raster_path)[0]}.png'
+        warp_task = task_graph.add_task(
+            func=subprocess.run,
+            args=([
+                'gdal_translate', '-of', 'PNG', clipped_raster_path,
+                png_path]),
+            target_path_list=[png_path],
+            dependent_task_list=[warp_task],
+            task_name=f'warp {png_path}')
 
 
 def extract_bounding_box(
