d65cdad39517ca85134d4fa10073de52d25bbd25
Rich Sharp <richpsharp@gmail.com>
2019-02-08 09:15:26 -0800

    using fetch if not downloaded

--- sentinel_data_fetch.py
+++ sentinel_data_fetch.py
@@ -130,7 +130,7 @@ def get_bounding_box_imagery(
 
             manifest_url = f'{url_prefix}/manifest.safe'
             manifest_path = os.path.join(granule_dir, 'manifest.safe')
-            if not os.path.exists(manifest_path):
+            if fetch_if_not_downloaded and not os.path.exists(manifest_path):
                 raise StopIteration('manifest not downloaded')
             manifest_task_fetch = task_graph.add_task(
                 func=urllib.request.urlretrieve,
