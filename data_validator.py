eeee684463d9447303a1d6efa88d14c42b299309
Rich Sharp <richpsharp@gmail.com>
2019-02-24 21:13:44 -0800

    color usernames

--- data_validator.py
+++ data_validator.py
@@ -258,6 +258,30 @@ def get_unvalidated_point():
         LOGGER.exception('exception in unvalidated')
         raise
 
+_KELLY_COLORS_HEX = [
+    '#FFB300',  # Vivid Yellow
+    '#803E75',  # Strong Purple
+    '#FF6800',  # Vivid Orange
+    '#A6BDD7',  # Very Light Blue
+    '#C10020',  # Vivid Red
+    '#CEA262',  # Grayish Yellow
+    '#817066',  # Medium Gray
+    # The following don't work well for people with defective color vision
+    '#007D34',  # Vivid Green
+    '#F6768E',  # Strong Purplish Pink
+    '#00538A',  # Strong Blue
+    '#FF7A5C',  # Strong Yellowish Pink
+    '#53377A',  # Strong Violet
+    '#FF8E00',  # Vivid Orange Yellow
+    '#B32851',  # Strong Purplish Red
+    '#F4C800',  # Vivid Greenish Yellow
+    '#7F180D',  # Strong Reddish Brown
+    '#93AA00',  # Vivid Yellowish Green
+    '#593315',  # Deep Yellowish Brown
+    '#F13A13',  # Vivid Reddish Orange
+    '#232C16',  # Dark Olive Green
+    ]
+
 
 @APP.route('/summary')
 def render_summary():
@@ -280,8 +304,8 @@ def render_summary():
                     'FROM validation_table '
                     'GROUP by username '
                     'ORDER BY count(username) DESC')
-                user_contribution_list = cursor.fetchall()
-
+                user_contribution_list = zip(
+                    _KELLY_COLORS_HEX, cursor.fetchall())
                 cursor.execute(
                     'SELECT source_point_wkt '
                     'FROM base_table '
